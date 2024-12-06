3ccdb4b9554ea854d4b19591b3412beedc732281
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-12-06 12:45:59 +0100

    feat: button UI

--- src/plugin/button.ts
+++ src/plugin/button.ts
@@ -2,11 +2,14 @@ import { makePersistentDraggable } from "./draggable";
 import type { InitialPosition, TooltipHtmlElement } from "./types";
 import { getElementByIdOr } from "./utils";
 
+import logo from "@/plugin/toolbox-logo.svg" with { type: "text" };
+
 const tooltipButton = (id: string, initialPosition?: InitialPosition) => {
   return getElementByIdOr(id, () => {
     const floatingButton: TooltipHtmlElement = document.createElement("div");
     floatingButton.classList.add("tooltip-button");
     floatingButton.id = id;
+    floatingButton.innerHTML = logo;
 
     document.body.appendChild(floatingButton);
     floatingButton.hide = () => {
