ec2888fac471bbe87142732c77cbd7d4756e2f6a
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-12-22 22:37:54 +0100

    feat: hide on long press

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -6,16 +6,34 @@ import {
 } from "./plugin/index";
 
 const MODAL_ID = "jkutkut/tooltip-modal";
-const tooltip = tooltipModal(MODAL_ID);
 const BUTTON_ID = "jkutkut/tooltip-button";
-const floatingButton = tooltipButton(BUTTON_ID);
+const LONG_PRESS_DELAY = 500;
 
 loadTooltipCss();
 
+const tooltip = tooltipModal(MODAL_ID);
+const floatingButton = tooltipButton(BUTTON_ID);
 floatingButton.addEventListener("click", () => {
   console.log("Floating button clicked!");
   tooltip.style.display = "flex";
   updateTooltip(tooltip, document.tmTools);
+});
+
+// long press
+let __tooltipLongPressTimeout: ReturnType<typeof setTimeout> | null = null;
+const disableTooltipLongPress = () => {
+  if (__tooltipLongPressTimeout) {
+    clearTimeout(__tooltipLongPressTimeout);
+    __tooltipLongPressTimeout = null;
+  }
+};
+floatingButton.addEventListener("mouseup", disableTooltipLongPress);
+floatingButton.addEventListener("mousemove", disableTooltipLongPress);
+floatingButton.addEventListener("mousedown", () => {
+  __tooltipLongPressTimeout = setTimeout(() => {
+    tooltip.style.display = "none";
+    floatingButton.style.display = "none";
+  }, LONG_PRESS_DELAY);
 });
 
 setTimeout(() => {
