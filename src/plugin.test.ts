65d440f597886e42e4d6a235bff63d8f714eac9d
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-01-12 23:31:40 +0100

    refactor: button actions now in props

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -7,39 +7,20 @@ import type {TooltipHtmlElement} from "./plugin/types";
 
 const MODAL_ID = "jkutkut/tooltip-modal";
 const BUTTON_ID = "jkutkut/tooltip-button";
-const LONG_PRESS_DELAY = 500;
 
 const tooltip = tooltipModal(MODAL_ID) as TooltipHtmlElement;
-const floatingButton = tooltipButton(BUTTON_ID) as TooltipHtmlElement;
-floatingButton.addEventListener("click", () => {
-  tooltip.show();
-  loadTooltipModal(tooltip, document.tmTools);
-});
-
-// long press
-let __tooltipLongPressTimeout: ReturnType<typeof setTimeout> | null = null;
-const disableTooltipLongPress = () => {
-  if (__tooltipLongPressTimeout) {
-    clearTimeout(__tooltipLongPressTimeout);
-    __tooltipLongPressTimeout = null;
-  }
-};
-floatingButton.addEventListener("mouseup", disableTooltipLongPress);
-floatingButton.addEventListener("mousemove", disableTooltipLongPress);
-floatingButton.addEventListener("mousedown", () => {
-  __tooltipLongPressTimeout = setTimeout(() => {
+const floatingButton = tooltipButton({
+  id: BUTTON_ID,
+  onClick: () => {
+    tooltip.show();
+    loadTooltipModal(tooltip, document.tmTools);
+  },
+  onLongClick: () => {
     tooltip.hide();
     floatingButton.hide();
-  }, LONG_PRESS_DELAY);
-});
-floatingButton.addEventListener("touchend", disableTooltipLongPress, { passive: false });
-floatingButton.addEventListener("touchmove", disableTooltipLongPress, { passive: false });
-floatingButton.addEventListener("touchstart", () => {
-  __tooltipLongPressTimeout = setTimeout(() => {
-    tooltip.hide();
-    floatingButton.hide();
-  }, LONG_PRESS_DELAY);
-}, { passive: false });
+  },
+  longClickDelay: 500
+}) as TooltipHtmlElement;
 
 setTimeout(() => {
   const click = new MouseEvent("click");
--- src/plugin/button.ts
+++ src/plugin/button.ts
@@ -6,7 +6,20 @@ import { getElementByIdOr, newDOMElement } from "./utils";
 
 import logo from "@/../dist/toolbox-logo.svg.o" with { type: "text" };
 
-const tooltipButton = (id: string, initialPosition?: InitialPosition) => {
+const DEF_LONG_PRESS_DELAY = 500;
+interface Props {
+  id: string;
+  initialPosition?: InitialPosition;
+  onClick?: () => void;
+  onLongClick?: () => void;
+  longClickDelay?: number;
+}
+
+const tooltipButton = ({
+  id, initialPosition,
+  onClick,
+  onLongClick, longClickDelay = DEF_LONG_PRESS_DELAY
+}: Props) => {
   loadTooltipCss();
   return getElementByIdOr(id, () => {
     const floatingButton = newDOMElement("div", ["tooltip-button"]) as TooltipHtmlElement;
@@ -17,12 +30,37 @@ const tooltipButton = (id: string, initialPosition?: InitialPosition) => {
     floatingButton.show = () => {
       floatingButton.style.display = "flex";
     };
+    if (onClick) {
+      floatingButton.addEventListener("click", onClick);
+    }
+    if (onLongClick) {
+      addLongPress(floatingButton, onLongClick, longClickDelay);
+    }
     floatingButton.innerHTML = lzDecompressUtf16(logo) || "";
 
     document.body.appendChild(floatingButton);
     makePersistentDraggable(floatingButton, initialPosition);
     return floatingButton;
   });
+};
+
+const addLongPress = (button: TooltipHtmlElement, onLongClick: () => void, longClickDelay: number) => {
+  let __tooltipLongPressTimeout: ReturnType<typeof setTimeout> | null = null;
+  const enableTooltipLongPress = () => {
+    __tooltipLongPressTimeout = setTimeout(onLongClick, longClickDelay);
+  };
+  const disableTooltipLongPress = () => {
+    if (__tooltipLongPressTimeout) {
+      clearTimeout(__tooltipLongPressTimeout);
+      __tooltipLongPressTimeout = null;
+    }
+  };
+  button.addEventListener("mouseup", disableTooltipLongPress);
+  button.addEventListener("mousemove", disableTooltipLongPress);
+  button.addEventListener("mousedown", enableTooltipLongPress);
+  button.addEventListener("touchend", disableTooltipLongPress, { passive: false });
+  button.addEventListener("touchmove", disableTooltipLongPress, { passive: false });
+  button.addEventListener("touchstart", enableTooltipLongPress, { passive: false });
 };
 
 export { tooltipButton };
