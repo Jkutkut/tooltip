938290dc65b6f4ce4a8f50ab956b788b31a17def
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2025-01-12 22:27:34 +0100

    feat: css can only be loaded once

--- src/plugin/cssLoader.ts
+++ src/plugin/cssLoader.ts
@@ -2,7 +2,12 @@ import {lzDecompressUtf16} from "./lz";
 import tooltipCssObj from "d/plugin.min.css.o" with { type: "text", encoding: "utf16" };
 
 const loadTooltipCss = () => {
+  const ID = "jkutkut/tooltip-css";
+  if (document.getElementById(ID)) {
+    return;
+  }
   const style = document.createElement("style");
+  style.id = ID;
   style.textContent = lzDecompressUtf16(tooltipCssObj);
   document.head.appendChild(style);
 };
